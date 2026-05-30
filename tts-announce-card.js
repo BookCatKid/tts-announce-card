2732f91a71075f019cda818d7124a5b046517f27
BookCatKid <99609593+BookCatKid@users.noreply.github.com>
2026-05-29 17:59:55 -0700

    finally actually fix tts

--- tts-announce-card.js
+++ tts-announce-card.js
@@ -567,7 +567,7 @@ class TTSAnnounceCard extends HTMLElement {
   }
 
   _serviceData(message, speakers) {
-    const ttsPlatform = "edge_tts";
+    const ttsPlatform = "tts.edge_tts_service_edge_tts";
     const volume = Number.isFinite(this._form.volume)
       ? this._form.volume
       : parseInt(this._form.volume, 10);
